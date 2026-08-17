fcf83c62002bc43032b56a36fab55c2e4293087f
Highskills and more Ltd. <michael@highskills.co.il>
2026-08-17 10:08:52 +0300

    update version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_ai_coursecreator';
-$plugin->version   = 2026062809;
+$plugin->version   = 2026062810;
 $plugin->requires  = 2023042400; // Moodle 4.2.
 $plugin->maturity  = MATURITY_ALPHA;
 $plugin->release   = '0.1.0';
